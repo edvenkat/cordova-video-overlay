bbf43d82082e4cfbc1272aed23bd0847983fbaba
Venka Tesh G <gvenkatesh225@gmail.com>
2020-05-11 22:40:39 +0530

    Update CameraSessionManager.m

--- src/ios/CameraSessionManager.m
+++ src/ios/CameraSessionManager.m
@@ -261,8 +261,8 @@
                               initWithURL:fileUrl
                               fileType:AVFileTypeQuickTimeMovie
                               error: &error];
-    //[self.assetWriterMyData addInput:self.assetWriterInput];
-    //self.assetWriterInput.expectsMediaDataInRealTime = YES;
+    [self.assetWriterMyData addInput:self.assetWriterInput];
+    self.assetWriterInput.expectsMediaDataInRealTime = YES;
   
     //
     self.audioWriterInput = [AVAssetWriterInput 
