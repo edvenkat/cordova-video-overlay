62cea13c4646cabb73219d9217f8fd37715b54b6
Venka Tesh G <gvenkatesh225@gmail.com>
2020-05-17 19:53:09 +0530

    Update CameraPreview.m

--- src/ios/CameraPreview.m
+++ src/ios/CameraPreview.m
@@ -575,17 +575,17 @@ if (audioInput) {
 }
  // file URL input
 // CMTime maxDuration = CMTimeMakeWithSeconds(1800, 1);
-output = [AVCaptureMovieFileOutput new];
+// output = [AVCaptureMovieFileOutput new];
       
 //         output.maxRecordedDuration = maxDuration;
 //         output.movieFragmentInterval = kCMTimeInvalid;
       
-if([captureSession canAddOutput:output]){
-    [captureSession addOutput:output];
-}
+// if([captureSession canAddOutput:output]){
+//     [captureSession addOutput:output];
+// }
   // Start recording
-// [self.sessionManager.myvideooutput startRecordingToOutputFileURL:fileURI recordingDelegate:self];
-  [output startRecordingToOutputFileURL:fileURI recordingDelegate:self];
+[self.sessionManager.myvideooutput startRecordingToOutputFileURL:fileURI recordingDelegate:self];
+  ///[output startRecordingToOutputFileURL:fileURI recordingDelegate:self];
 //[captureSession startRunning];
   //
   [self.sessionManager updateOrientation:AVCaptureVideoOrientationPortrait];
@@ -625,8 +625,8 @@ if([captureSession canAddOutput:output]){
   
 //     [self.sessionManager.session stopRunning];
 //    [captureSession stopRunning];
-   [output stopRecording];
-    //[self.sessionManager.myvideooutput stopRecording];
+  // [output stopRecording];
+    [self.sessionManager.myvideooutput stopRecording];
     self.cameraRenderController.view.alpha = 0;
 
     NSFileManager *fileManager = [NSFileManager defaultManager];
